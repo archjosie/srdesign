219b339d2023a7fccd4ba2b2fdf3b1f32be679e6
jmpohl <josiahpohl@live.com>
2017-02-08 13:54:08 -0700

    Journal Update

--- fresneleqns/main.cpp
+++ fresneleqns/main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 double rTE(double n, double theta, vector<double> kvec){
-    vector<double> nvec(3);
+    vector<double> nvec(0);
     nvec.push_back(-sin(theta));
     nvec.push_back(0);
     nvec.push_back(cos(theta));
@@ -19,29 +19,49 @@ double rTE(double n, double theta, vector<double> kvec){
     return coeff;
 } 
 
-double rTEb(double n, double theta, vector<double> kvec){
-    vector<double> nvec(3);
+double rTM(double n, double theta, vector<double> kvec){
+    vector<double> nvec(0);
     nvec.push_back(-sin(theta));
     nvec.push_back(0);
     nvec.push_back(cos(theta));
 
     double beta= acos(nvec.at(0)*kvec.at(0)+nvec.at(1)*kvec.at(1)+nvec.at(2)*kvec.at(2));
-    return beta;
-}
+    double coeff= (pow(n,2)*cos(beta)-sqrt(pow(n,2)-pow(sin(beta),2)))/(pow(n,2)*cos(beta)+sqrt(pow(n,2)-pow(sin(beta),2)));
+    return coeff;
+} 
+
+double tTE(double n, double theta, vector<double> kvec){
+    vector<double> nvec(0);
+    nvec.push_back(-sin(theta));
+    nvec.push_back(0);
+    nvec.push_back(cos(theta));
+
+    double beta= acos(nvec.at(0)*kvec.at(0)+nvec.at(1)*kvec.at(1)+nvec.at(2)*kvec.at(2));
+    double coeff= (2*cos(beta))/(cos(beta)+sqrt(pow(n,2)-pow(sin(beta),2)));
+    return coeff;
+} 
+
+double tTE(double n, double theta, vector<double> kvec){
+    vector<double> nvec(0);
+    nvec.push_back(-sin(theta));
+    nvec.push_back(0);
+    nvec.push_back(cos(theta));
+
+    double beta= acos(nvec.at(0)*kvec.at(0)+nvec.at(1)*kvec.at(1)+nvec.at(2)*kvec.at(2));
+    double coeff= (2*n*cos(beta))/(pow(n,2)*cos(beta)+sqrt(pow(n,2)-pow(sin(beta),2)));
+    return coeff;
+} 
 
 int main(){
 //    double n = 1.6592;
     double n = .6;
     double theta = 30*PI/180;
-    vector<double> kvec(3);
+    vector<double> kvec(0);
     kvec.push_back(0.0);
     kvec.push_back(0.0);
     kvec.push_back(1.0);
 
-    cout << kvec.at(3) << endl;
     cout << rTE(n, theta, kvec) << endl;
-    cout << rTEb(n, theta, kvec) << endl;
-
 	
     return 0;
 }
